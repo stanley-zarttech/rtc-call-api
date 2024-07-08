4307fddeae35554f86394ef975858704020fe609
Stanley Joel N. <n.stanley@zarttech.com>
2024-07-08 17:07:28 +0100

    fixes on postbuild

--- postbuild.js
+++ postbuild.js
@@ -5,14 +5,16 @@ dotenv.config();
 const filePath = "./public/rtccallapi_external.js";
 
 const socketUrl = process.env.SOCKET_URL;
-const token = process.env.TOKEN;
+const token = process.env.TOKEN || "replacement done";
 const domain = process.env.DOMAIN;
 
 console.log("token: ", token);
 const fileContent = fs.readFileSync(filePath, "utf8");
+console.log("content: ", fileContent);
 const updatedContent = fileContent
   //   .replace(/SOCKET_URL_PLACEHOLDER/g, socketUrl)
-  .replace(/replace-token/, token);
+  .replace(/replace-token/i, token);
 //   .replace(/DOMAIN_PLACEHOLDER/g, domain);
+console.log("file content again: ", updatedContent);
 
 fs.writeFileSync(filePath, updatedContent);
